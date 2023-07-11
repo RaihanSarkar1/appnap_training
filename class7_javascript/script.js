091b782dd91abb960e08a6012b8add5ae72b8b4f
tRaihanSarkar <t.raihansarkar@gmail.com>
2023-07-11 20:40:45 +0600

    Update to calculator.html

--- class7_javascript/script.js
+++ class7_javascript/script.js
@@ -9,12 +9,23 @@ var operand2 = 0;
 buttons.forEach((button) => {
     button.addEventListener("click", (event) => {
         if (event.target.innerHTML === "AC") {
-            operand.innerHTML = 0;
+            operand.innerHTML = null;
         } else if (event.target.innerHTML === "=") {
             
+
+        } else if (event.target.innerHTML === String.fromCharCode(8592)){   //The 8592 is the unicode for back arrow
+            operand.innerHTML = operand.innerHTML.slice(0, -1);
+
+        } else if (event.target.innerHTML === String.fromCharCode(8592) && operand.innerHTML === null){
+            operand.innerHTML = 0;
+            
+        } else if (event.target.innerHTML === "=") {
+            
+        } else if (operand.innerHTML === 0) {
+            operand.innerHTML = operand.innerHTML.slice(0, -1);
+            operand.innerHTML += event.target.innerHTML;
         } else {
             operand.innerHTML += event.target.innerHTML;
-            console.log(event.target.innerHTML);
         }
     })
 })
