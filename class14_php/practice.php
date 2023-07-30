7f208243797fff66183b23f33a7e9ed7eabce9b0
tRaihanSarkar <t.raihansarkar@gmail.com>
2023-07-30 21:39:17 +0600

    Added content

--- class14_php/practice.php
+++ class14_php/practice.php
@@ -39,7 +39,7 @@ function thisFun() {
 
 function myTest() {
     static $x = 0;
-    echo $x;
+    echo $x."<br>";
     $x++;
 }
 
@@ -47,3 +47,56 @@ myTest();
 myTest();
 myTest();
 
+
+// Arrays
+// In php the array() function is used to create arrays
+$cars = array("toyota","subaru","Volkswagon","Lamborgini");
+echo "I like ".$cars[0]." ,".$cars[1]." ,".$cars[2]." ,".$cars[3];
+// The count() function is used return the length of the array.
+echo "<br>".count($cars);
+
+// Loops
+// While loop runs while a condition is true
+$x = 0;
+while( $x <= 5) {
+    echo "<br>The number x is ".$x;
+    $x++;
+}
+
+// do while loop executes once and then repeats the block while a condition is true
+$x = 1;
+do {
+    echo "<br>The number x is ".$x;
+    $x++;
+} while ($x <= 5);
+
+// the for loop is used when you know how many times a loop should run
+for ($x = 0; $x<=10; $x++) {
+    echo "<br> The number x is ".$x;
+}
+
+// The foreach loop - loops through a block of code for each element of an array.
+// it only works on arrays, and is used to loop through each key/value pair in array.
+$colors = array("red", "green", "blue", "yellow");
+
+foreach($colors as $value){
+    echo "<br>".$value;
+}
+
+// Associative Arrays
+$age = array("Peter"=>"30","John"=>"28","Tooth"=>"30");
+
+echo "<br>Peter is ".$age['Peter']." years old";
+
+// Looping through associative array
+foreach($age as $key => $value) {
+    echo "<br>Key-".$key." Value-".$value;
+}
+
+// Array functions
+// sort() - sorts array in asccending order
+// rsort() - sorts array in descending order
+// asort() - sorts associative arrays in ascending order, according to the value
+// ksort() - sorts associative arrays in ascending order, according to the key
+// arsort() - sorts associative arrays in descending order, according to the value
+// krsort() - sorts associative array in descending order, according to the key
